3b849d8b7c7cfabb15bc2ed87df33c226aa73f0a
Matt Belisle <matthew.belisle@workiva.com>
2018-12-08 19:19:29 -0600

    Trying name again

--- <img onerror="alert(`xss on ${document.domain}`);" src="x" .go
+++ <img onerror="alert(`xss on ${document.domain}`);" src="x" .go
@@ -1,1 +1,9 @@
 package main
+
+import "fmt"
+
+const message = "Hello, 世界"
+
+func main() {
+	fmt.Println(message)
+}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-const message = "Hello, 世界"
-
-func main() {
-	fmt.Println(message)
-}
